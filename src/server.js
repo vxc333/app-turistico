7a4a2cc87eb260763c1d51c6c57cc6363db0affc
vxc333 <you@example.com>
2024-08-03 20:26:01 -0300

    Fix Bug

--- src/server.js
+++ src/server.js
@@ -1,13 +1,16 @@
 const express = require('express');
-const app = express();
-const port = 3000;
 const routes = require('./routes');
 const sequelize = require('./config/database');
 const Destination = require('./models/Destination');
 const Attraction = require('./models/Attraction');
+const cors = require("cors");
+
+const app = express();
+const port = 3000;
 
 app.use(express.json());
 app.use('/api', routes);
+app.use(cors());
 
 sequelize.sync()
   .then(() => {
